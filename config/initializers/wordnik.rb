d32a8271db103939311d098e9bcb0f4e546e0388
zeke <zeke@sikelianos.com>
2012-02-12 10:57:23 -0800

    Added not about API key

--- config/initializers/wordnik.rb
+++ config/initializers/wordnik.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 
 Wordnik.configure do |config|
-  config.api_key = 'b39ee8d5f05d0f566a0080b4c310ceddf5dc5f7606a616f53'
+  config.api_key = 'YOUR-API-KEY-HERE'
   config.logger = Logger.new('/dev/null')
 end
