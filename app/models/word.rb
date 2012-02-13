58b0aed822a7a44cde40806c25f37aabd72f569e
zeke <zeke@sikelianos.com>
2012-02-13 11:47:38 -0800

    Added Word.random method and updated documentation in README

--- app/models/word.rb
+++ app/models/word.rb
@@ -9,5 +9,25 @@ class Word
   def definitions
     Wordnik.word.get_definitions(self.name)
   end
+
+  # Get one or many random words
+  # Word.random(:limit => 10)
+  # Word.random
+  def self.random(options={})
+
+    # Defaults
+    options.reverse_merge!(
+      :min_corpus_count => 1000,
+      :min_dictionary_count => 5,
+      :limit => 1
+    )
+    
+    # Get back an array of hashes that look like {"id"=>92367, "word"=>"resist"} 
+    # and turn them into Word objects:
+    words = Wordnik.words.get_random_words(options).map{|obj| Word.new(obj['word']) }
+    
+    # Return one or many?
+    options[:limit] == 1 ? words.first : words
+  end
   
 end
