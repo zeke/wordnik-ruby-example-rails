71a3f660d2002e7ee42943bcda9136e5677aa614
zeke <zeke@sikelianos.com>
2012-02-13 11:47:23 -0800

    Use ENVIRONMENT variable for wordnik API key

--- config/initializers/wordnik.rb
+++ config/initializers/wordnik.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 
 Wordnik.configure do |config|
-  config.api_key = 'YOUR-API-KEY-HERE'
+  config.api_key = ENV['WORDNIK_API_KEY']
   config.logger = Logger.new('/dev/null')
 end
